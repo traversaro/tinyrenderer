def728c0c8144242ad41cfd865a3208204814fc6
Erwin Coumans <erwin.coumans@gmail.com>
2021-09-22 21:33:49 -0700

    add simple examples

--- python/examples/example_mesh.py
+++ python/examples/example_mesh.py
@@ -1,10 +1,7 @@
-print("import pytinyrenderer")
 import pytinyrenderer
-#pytinyrenderer.file_open_dialog("test")
 import numpy as np
 from numpngw import write_apng
 from PIL import Image
-print("end import")
 
 import math
 scene = pytinyrenderer.TinySceneRenderer()
@@ -34,25 +31,8 @@ class TextureRGB888:
   
 
 texture = TextureRGB888()
-print("load texture")
 texture.load("tex256.png")
-print("create capsules")
-capx = scene.create_capsule(0.1,0.4,0, texture.pixels, texture.width, texture.height)
-capy = scene.create_capsule(0.1,0.4,1, [255,0,0,0,255,0,0,0,255,255,255,255],2,2)
-capz = scene.create_capsule(0.1,0.4,2, [255,0,0,0,255,0,0,0,255,255,255,255],2,2)
 
-
-
-if 0:
-  
-
-  cube = scene.create_cube([0.5,0.5,0.03], pixels, width, height, 16.)
-
-
-
-
-
-#img = scene.get_camera_image(640,480,[capx,capy,capz])
 
 NEAR_PLANE = 0.01
 FAR_PLANE = 1000
@@ -71,14 +51,6 @@ view_matrix = pytinyrenderer.compute_view_matrix(eye, target, up)
 
 
 
-
-
-#vertices =np.array([[1.000000, -1.000000, 0.000000], 
-#            [1.000000, 1.000000, 0.000000],
-#            [-1.000000, 1.000000, 0.000000], 
-#            [-1.000000,  -1.000000, 0.000000]]).flatten().tolist()
-
-
 vertices =np.array([[100.000000, -100.000000, 0.000000], 
             [100.000000, 100.000000, 0.000000],
             [-100.000000, 100.000000, 0.000000], 
@@ -95,55 +67,18 @@ indices = [
     0,    2,    3]
     
 
-if 0:
-  import time
-  import pybullet as p
-  p.connect(p.GUI)
-
-  visualShapeId = p.createVisualShape(shapeType=p.GEOM_MESH,
-                                      rgbaColor=[1, 1, 1, 1],
-                                      vertices=vertices,
-                                      indices=indices,
-                                      uvs=uvs,
-                                      normals=normals)
-
-  texUid = p.loadTexture("tex256.png")#checker_blue.png")
-
-
-  bodyUid = p.createMultiBody(baseMass=0,
-                                baseInertialFramePosition=[0, 0, 0],
-                                baseVisualShapeIndex=visualShapeId,
-                                basePosition=[0,0,0],
-                                useMaximalCoordinates=True)
-                                  
-  p.changeVisualShape(bodyUid, -1, textureUniqueId=texUid)
-
-  #for i in range (1000):#while 1:
-  width,height,px,_,_ = p.getCameraImage(640,480, projectionMatrix=projection_matrix, viewMatrix=view_matrix)
-  rgb_array = np.reshape(np.array(px,dtype=np.uint8), (height, width, -1))
-
-  images=[]
-  images.append(rgb_array)
-  write_apng('tinyanim_pb.png', images, delay=20)
-
 plane_model = scene.create_mesh(vertices.flatten().tolist(), normals.flatten().tolist(), uvs.flatten().tolist(), indices, texture.pixels, texture.width, texture.height, 1.)
 
 plane_instance = scene.create_object_instance(plane_model)
-#scene.set_object_position(plane_instance,[-10,-2,-431])
+#scene.set_object_position(plane_instance,[-10,-2,-131])
 scene.set_object_orientation(plane_instance,[0,0,0,1])
-scene.set_object_local_scaling(plane_instance,[0.01,0.01,0.01])
+#scene.set_object_local_scaling(plane_instance,[0.01,0.01,0.01])
 
-#int create_object_instance(int model_uid, const std::vector<float>& position, const std::vector<float>& orientation, const std::vector<float> color, int user_unique_id)
-print("plane_instance=",plane_instance)
-
-print("get camera image")    
 img = scene.get_camera_image(640,480,[plane_instance], view_matrix, projection_matrix)
-print("reshape")    
 rgb_array = np.reshape(np.array(img.rgb,dtype=np.uint8), (img.height, img.width, -1))
 
 images=[]
 images.append(rgb_array)
 
-print("writing png")    
 write_apng('tinyanim2_tds.png', images, delay=20)
-print("done")
+
