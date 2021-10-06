440df886d5556798cf11f3d1538042a10572b465
Erwin Coumans <erwincoumans@google.com>
2021-10-06 16:55:06 -0700

    fix example_mesh.py after API change

--- examples/python/example_mesh.py
+++ examples/python/example_mesh.py
@@ -6,6 +6,14 @@ from PIL import Image
 import math
 scene = pytinyrenderer.TinySceneRenderer()
 
+width = 640
+height = 480
+eye = [2., 4., 1.]
+target = [0., 0., 0.]
+light = pytinyrenderer.TinyRenderLight()
+camera = pytinyrenderer.TinyRenderCamera(viewWidth=width, viewHeight=height,
+                                          position=eye, target=target)
+
 class TextureRGB888:
   def __init__(self):
     self.pixels = [
@@ -34,27 +42,6 @@ texture = TextureRGB888()
 texture.load("tex256.png")
 
 
-NEAR_PLANE = 0.01
-FAR_PLANE = 1000
-HFOV = 58.0
-VFOV = 45.0
-left=-math.tan(math.pi*HFOV/360.0)*NEAR_PLANE
-right=math.tan(math.pi*HFOV/360.0)*NEAR_PLANE
-bottom=-math.tan(math.pi*VFOV/360.0)*NEAR_PLANE
-top=math.tan(math.pi*VFOV/360.0)*NEAR_PLANE
-projection_matrix = pytinyrenderer.compute_projection_matrix( left, right, bottom, top, NEAR_PLANE, FAR_PLANE)
-
-up = [0.,0.,1.]
-eye = [2.,-4.,11.]
-target = [0.,0.,1.]
-view_matrix = pytinyrenderer.compute_view_matrix(eye, target, up)
-
-
-
-vertices =np.array([[100.000000, -100.000000, 0.000000], 
-            [100.000000, 100.000000, 0.000000],
-            [-100.000000, 100.000000, 0.000000], 
-            [-100.000000,  -100.000000, 0.000000]])#.flatten().tolist()
 #vertices = vertices*0.01
 normals = np.array([[0.000000, 0.000000, 1.000000], [0.000000, 0.000000, 1.000000],
            [0.000000, 0.000000, 1.000000], [0.000000, 0.000000, 1.000000]])#.flatten().tolist()
@@ -67,6 +54,11 @@ indices = [
     0,    2,    3]
     
 
+vertices =np.array([[100.000000, -100.000000, 0.000000], 
+            [100.000000, 100.000000, 0.000000],
+            [-100.000000, 100.000000, 0.000000], 
+            [-100.000000,  -100.000000, 0.000000]])#.flatten().tolist()
+
 plane_model = scene.create_mesh(vertices.flatten().tolist(), normals.flatten().tolist(), uvs.flatten().tolist(), indices, texture.pixels, texture.width, texture.height, 1.)
 
 plane_instance = scene.create_object_instance(plane_model)
@@ -74,7 +66,7 @@ plane_instance = scene.create_object_instance(plane_model)
 scene.set_object_orientation(plane_instance,[0,0,0,1])
 #scene.set_object_local_scaling(plane_instance,[0.01,0.01,0.01])
 
-img = scene.get_camera_image(640,480,[plane_instance], view_matrix, projection_matrix)
+img = scene.get_camera_image([plane_instance], light, camera)
 rgb_array = np.reshape(np.array(img.rgb,dtype=np.uint8), (img.height, img.width, -1))
 
 images=[]
