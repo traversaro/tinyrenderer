089eca64427a03d16a01b6ea7ecbdbefe8dd9556
Erwin Coumans <erwincoumans@google.com>
2021-10-20 18:32:13 +0000

    bump version

--- setup.py
+++ setup.py
@@ -186,7 +186,7 @@ extensions.append(pytinyrenderer_ext)
 
 setup(
     name='pytinyrenderer',
-    version='0.0.11',
+    version='0.0.12',
     description=
     'Python bindings for Tiny Renderer',
     long_description=
